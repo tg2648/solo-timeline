c91527705144ad93292f894a9a3095bbeb39f949
tg2648 <45052145+tg2648@users.noreply.github.com>
2022-02-17 20:19:19 -0500

    drag and drop init

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,29 @@
 import { useState } from 'react'
 import logo from './logo.svg'
+import { DragDropContext, DragStart, DropResult } from 'react-beautiful-dnd';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  function onDragStart(start: DragStart) {
+    document.body.style.color = 'orange';
+    document.body.style.transition = 'background-color 0.2s ease';
+  }
+
+  function onDragEnd(result: DropResult) {
+    document.body.style.color = 'inherit';
+    document.body.style.backgroundColor = 'inherit';
+
+    const { destination, source, draggableId } = result;
+  }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <DragDropContext
+      onDragEnd={onDragEnd}
+      onDragStart={onDragStart}
+    >
+
+    </DragDropContext>
   )
 }
 
