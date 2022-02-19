cf6da906ede20e85fe9e8025c62b4296bc56f66a
tg2648 <45052145+tg2648@users.noreply.github.com>
2022-02-18 22:29:10 -0500

    Data

---
+++ src/initial_data.ts
@@ -0,0 +1,42 @@
+export type CardType = {
+      id: string,
+      content: string,
+}
+
+export type HandType = {
+   id: string,
+   cardIds: string[],
+}
+
+type GameData = {
+   cards: {
+      [index: string]: CardType
+   },
+   hands: {
+      [index: string]: HandType,
+   },
+   handOrder: string[]
+}
+
+
+export const initialGameData: GameData = {
+   cards: {
+      'card-1': { id: 'card-1', content: 'Card 1'},
+      'card-2': { id: 'card-2', content: 'Card 2'},
+      'card-3': { id: 'card-3', content: 'Card 3'},
+      'card-4': { id: 'card-4', content: 'Card 4'},
+   },
+
+   hands: {
+      'player': {
+         id: 'player',
+         cardIds: ['card-1', 'card-3']
+      },
+      'timeline': {
+         id: 'timeline',
+         cardIds: ['card-2']
+      },
+   },
+
+   handOrder: ['timeline', 'player'],
+}
