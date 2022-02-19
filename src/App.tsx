e2b6533ad3678570636375d8269bad53c7253051
tg2648 <45052145+tg2648@users.noreply.github.com>
2022-02-18 22:47:54 -0500

    App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import { useState } from 'react'
-import logo from './logo.svg'
 import { DragDropContext, DragStart, DropResult } from 'react-beautiful-dnd';
+
 import './App.css'
+import { initialGameData, HandType, GameData } from './initial_data'
+import { Hand } from './Hand'
+
 
 function App() {
+
+  const [gameData, setGameData] = useState(initialGameData)
 
   function onDragStart(start: DragStart) {
     document.body.style.color = 'orange';
@@ -15,14 +20,54 @@ function App() {
     document.body.style.backgroundColor = 'inherit';
 
     const { destination, source, draggableId } = result;
+    if (!destination) {
+      return
+    }
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      // Dropped back into the same location
+      return
+    }
+
+    const hand = gameData.hands[source.droppableId]
+    const newCardIds = Array.from(hand.cardIds)
+
+    newCardIds.splice(source.index, 1)
+    newCardIds.splice(destination.index, 0, draggableId)
+
+    const newHand: HandType = {
+      ...hand,
+      cardIds: newCardIds
+    }
+
+    setGameData((oldData) => {
+      return {
+        ...oldData,
+        hands: {
+          ...oldData.hands,
+          [newHand.id]: newHand,
+        }
+      }
+    })
   }
+
 
   return (
     <DragDropContext
-      onDragEnd={onDragEnd}
       onDragStart={onDragStart}
+      onDragEnd={onDragEnd}
     >
+      {gameData.handOrder.map((handId) => {
+        const hand = gameData.hands[handId]
+        const cards = hand.cardIds.map(cardId => gameData.cards[cardId])
 
+        return (
+          <Hand key={hand.id} hand={hand} cards={cards} />
+        )
+      })}
     </DragDropContext>
   )
 }
